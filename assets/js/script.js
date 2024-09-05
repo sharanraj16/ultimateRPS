edc7227910dcfaf1daaa67af5737aca931adbc26
sharanraj sivakumar <sharanrobo@googlemail.com>
2024-09-05 08:25:16 +0000

    updating html for pp2 resubmit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Background music element and volume control button
+    const backgroundMusic = document.getElementById("background-music");
+    const volumeBtn = document.getElementById("volume-btn");
+    let isMuted = false;
+
+    // Sound elements
+    const clickSound = document.getElementById("click-sound");
+    const winSound = document.getElementById("win-sound");
+    const loseSound = document.getElementById("lose-sound");
+}
+
 // Function to start the game and display welcome message
 function startGame() {
     playSound(clickSound);
